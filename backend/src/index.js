9d4d2e5483eafcf3d306e7f617a923d95406d13f
bhanu-2208 <pbhanuprakashreddy2208@gmail.com>
2025-08-04 16:56:48 +0530

    Project Completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const __dirname=path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173",process.env.CLIENT_URL],
     credentials:true,
 }))
 
@@ -22,7 +22,7 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,'../fronetnd/dist')))
+    app.use(express.static(path.join(__dirname,'../frontend/dist')))
 
     app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
